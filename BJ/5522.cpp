4c610c2a93bbddcca4c688d76cfe17c288ec0cd8
Jeounghun Choi <glenjh0913@gmail.com>
2022-10-01 09:44:01 +0900

    10_01

---
+++ BJ/5522.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int sum = 0, input;
+    for(int i = 0 ; i < 5 ; i++){
+        cin >> input;
+        sum += input;
+    }
+    cout << sum;
+    return 0;
+}
