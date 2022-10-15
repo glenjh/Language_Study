db1dc52f5885a4555fbdfc24333d50ae542eba91
Jeounghun Choi <glenjh0913@gmail.com>
2022-10-15 20:17:03 +0900

    10_15

---
+++ BJ/20492.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << (n * 78) / 100 << '\n';
+    cout << n - (((n*2)/10)*22)/100 << '\n'; 
+    return 0;
+}
---
+++ BJ/25238.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin >> a >> b;
+    c = a*(100 - b) / 100;
+    if(c  >= 100){ cout << 0; }
+    else { cout << 1; }
+    return 0;
+}
