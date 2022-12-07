4fa70cd46592e3f23f019aea33af7f42e4389be6
CJH <glenjh0913@gmail.com>
2022-12-07 11:13:39 +0900

    12_05

---
+++ BJ/26082.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b , c;
+    cin >> a >> b >> c;
+
+    int temp = b / a;
+
+    cout << (3 * temp) * c << '\n';
+    return 0;
+}
